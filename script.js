8651e6c82425026628ce78766700e92bd0eca6b4
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-01 17:06:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 const parseCode = (str) => {
   // your code here
+	// let firstName="";
+	// let lastName="";
+	// let id="";
+	// for(let i=0;i<str.length;i++){
+	// 	if(firstName==="" &&  lastName==="" && id===""){
+	// 		if(str.charAt(i)!=0){
+	// 			firstName+=str.charAt(i);
+	// 		}
+	// 	}else if(firstName!="" &&lastName==="" && id===""){
+	// 		if(str.charAt(i)!=0){
+	// 			lastName+=str.charAt(i);
+	// 		}
+	// 	}else{
+	// 		if(str.charAt(i)!=0){
+	// 			id+=str.charAt(i);
+	// 		}
+	// 	}
+	// }
+	//  let obj={
+	// 	 firstName:firstName,
+	// 	 lastName:lastName,
+	// 	 id:id
+	//  }
+	// return obj;
+
+	let result = str.split("0").filter(Boolean);
+	let obj={
+		 firstName:result[0],
+		 lastName:result[1],
+		 id:result[2]
+	 }
+	return obj;
 };
 
 // Do not change the code below
